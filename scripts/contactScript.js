416d6e47cfa22a299e20ee8a11fc4d78ba30750a
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2019-03-27 18:23:59 +0000

    Started group script + improved contact script.

--- scripts/contactScript.js
+++ scripts/contactScript.js
@@ -5,13 +5,14 @@ let contactBtn = document.getElementById('addContact');
 contactBtn.addEventListener('click', addContactFunction);
 
 function addContactFunction(){
-  document.getElementById('contactsOverview').style.display = 'none';
+  document.getElementById('contactList').style.display = 'none';
   document.getElementById('addContactContainer').style.display = 'block';
 }
 
 let submitContactButton = document.getElementById('submitContactInformation');
 submitContactButton.addEventListener('click', submitContactFunction);
 
+//Function for submitting a new contact:
 function submitContactFunction(){
   let contactName = document.getElementById('contactNameInput').value;
   let contactPhone = document.getElementById('contactPhoneInput').value;
@@ -27,17 +28,39 @@ function submitContactFunction(){
   } else if(contactDescription.length > 140){
     alert('Contact Descriptions cannot exceed 140 characters in length.');
   } else {
-
     let newContact = document.createElement('div');
     let contactTag = document.createElement('p');
     contactTag.textContent = contactName;
     newContact.className = 'contact';
     newContact.appendChild(contactTag);
-    document.getElementById('contactsOverview').appendChild(newContact);
-    document.getElementById('contactsOverview').style.display = 'block';
+    document.getElementById('contactList').appendChild(newContact);
+    document.getElementById('contactList').style.display = 'block';
     document.getElementById('addContactContainer').style.display = 'none';
   }
+}
 
+let addGroupButton = document.getElementById('addGroup');
+addGroupButton.addEventListener('click', addGroupFunction)
+let submitGroupButton = document.getElementById('submitGroupInformation');
+submitGroupButton.addEventListener('click', submitGroupFunction)
+
+function addGroupFunction(){
+  document.getElementById('addGroupContainer').style.display = 'block';
+  document.getElementById('groupList').style.display = 'none';
+  document.getElementById('contactList').style.display = 'block';
+  document.getElementById('addContactContainer').style.display = 'none';
+}
+
+
+//Function for submitting a new group:
+function submitGroupFunction(){
+
+
+  let groupName = document.getElementById('groupNameInput').value;
+  let groupDescription = document.getElementById('groupDescriptionInput').value;
+  let groupMembers = document.getElementById('groupMembersInput').value;
+
+  alert(groupName + groupDescription + groupMembers);
 
 
 
