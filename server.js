fa4b0e5edde13462f2da9bb4378ca3da8f4acaa4
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2019-04-01 15:14:25 +0100

    node js server

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css/style.css',function(req,res) {
+  res.sendFile(__dirname + '/css/style.css');
+});
+
+
+app.listen(8080, () => console.log('App Listening on port 8080.'))
