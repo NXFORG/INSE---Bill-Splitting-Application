ae39b5e2e488e3a46cac8fa42029131fa8d9f91a
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2019-04-01 23:34:20 +0100

    AJAX

--- public/scripts/voucherScript.js
+++ public/scripts/voucherScript.js
@@ -1,13 +1,16 @@
 'use strict';
 
-
 $.ajax({
   type: 'GET',
   url: 'vouchers.json',
-  datatype: 'json',
-  success: function(voucherData){
-    $.each(index){
-      
-    }
+  dataType: 'json',
+  success: function(data){
+    $.each(data, function(index, item){
+        let itemString = JSON.stringify(item);
+        let newVoucher = document.createElement('div');
+        newVoucher.className = 'voucher';
+        $(newVoucher).append($('<p>').text(itemString));
+        $('#voucherContainer').append(newVoucher);
+    })
   }
 });
