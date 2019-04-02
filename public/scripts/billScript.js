034b215e5ed6f593812041ad7ebb72cfa45af964
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2019-04-02 19:37:21 +0100

    Quick Split script

--- public/scripts/billScript.js
+++ public/scripts/billScript.js
@@ -1,17 +1,35 @@
 'use strict';
+$('#amountContainer').hide();
+$('#quickChoice').hide();
 
-//Script for the bill split:
-$('#confirmAmountBtn').click(function(){
-  //Checks data type:
-  if(isNaN(amountInput.value)){
-    alert('Error must enter a valid number.');
+
+
+$('#submitSplitType').click(function(){
+  $('#splitChoice').hide();
+  let selectorValue = document.getElementById('splitDecisionSelect').value;
+
+  if(selectorValue == 'quickSplit'){
+    $('#quickChoice').show();
+    $('#quickCalc').click(function(){
+      let quickCost = document.getElementById('quickCost').value;
+      let quickPeople = document.getElementById('quickQuantity').value;
+      let finalCost = validateCostInput(quickCost/quickPeople);
+      $('#quickCostDisplay').text('Cost per person: £ ' + finalCost);
+    });
   } else {
-    //Checks range:
-    if(amountInput.value <= 0 || amountInput.value > 5000){
-      alert('Error must enter a in range 1-5000.');
-    } else {
-      //Rounds to 2dp:
-       let finalValue = Math.round(amountInput.value * 100) / 100;
-    }
+    $('#amountContainer').show();
   }
 });
+
+
+
+function validateCostInput(costInput){
+  if(isNaN(costInput)){
+    alert('Error must enter a valid number.');
+  } else if(costInput <= 0 || costInput > 5000){
+      alert('Error must enter a in range 1-5000.');
+    } else {
+       costInput = Math.round(costInput * 100) / 100;
+       return costInput;
+    }
+  }
