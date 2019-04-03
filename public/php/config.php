64d86ef5cd3f2bd30d26f370cc5e402f94f3b505
KurtR4 <45108906+KurtR4@users.noreply.github.com>
2019-04-03 11:11:30 +0100

    Add files via upload
    configure php for api.

---
+++ public/php/config.php
@@ -0,0 +1,6 @@
+<?php
+const DBHOST = '127.0.0.1'; // hostname IP address of DB server
+const DBNAME = 'slice'; // name of database
+const DBUSER = 'slice'; // permission for user
+const DBPW = 'sliceapp'; // user password
+?> 
