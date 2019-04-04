7d99354432f5fca2d47f1db6c5bb17df90addfa6
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2019-04-04 01:23:34 +0100

    Update billScript.js

--- public/scripts/billScript.js
+++ public/scripts/billScript.js
@@ -4,22 +4,68 @@ $('#quickChoice').hide();
 
 
 
+
 $('#submitSplitType').click(function(){
   $('#splitChoice').hide();
   let selectorValue = document.getElementById('splitDecisionSelect').value;
 
   if(selectorValue == 'quickSplit'){
-    $('#quickChoice').show();
-    $('#quickCalc').click(function(){
-      let quickCost = document.getElementById('quickCost').value;
-      let quickPeople = document.getElementById('quickQuantity').value;
-      let finalCost = validateCostInput(quickCost/quickPeople);
-      $('#quickCostDisplay').text('Cost per person: £ ' + finalCost);
-    });
+    quickSplit();
+
   } else {
-    $('#amountContainer').show();
+    groupSplit();
   }
 });
+
+function quickSplit(){
+  $('#quickChoice').show();
+  $('#quickCalc').click(function(){
+    const quickCost = document.getElementById('quickCost').value;
+    const quickPeople = document.getElementById('quickQuantity').value;
+    const finalCost = validateCostInput(quickCost/quickPeople);
+    $('#quickCostDisplay').text('Cost per person: £ ' + finalCost);
+  });
+}
+
+function groupSplit(){
+  //Ajax to load the groups into the drop down box:
+    $('#amountContainer').show();
+    $.ajax({
+      type: 'GET',
+      url: './JSONstorage/groups.json',
+      dataType: 'json',
+      success: function(data){
+        $.each(data, function(index, item){
+            let itemString = JSON.stringify(item);
+            itemString = itemString.replace(/\"([^(\")"]+)\":/g,"$1:");
+            const option = document.createElement('option');
+            option.text = item.Name;
+            $('#splitGroupSelect').append(option);
+        })
+      }
+    });
+
+  //Event listener for the button box:
+  $('#confirmAmountBtn').click(function(){
+    let dropText = document.getElementById('splitGroupSelect').value;
+    $.ajax({
+      type: 'GET',
+      url: './JSONstorage/groups.json',
+      dataType: 'json',
+      success: function(data){
+        $.each(data, function(index, item){
+            if(item.Name === dropText){
+              let cost = parseFloat(document.getElementById('amountCostInput').value);
+              let size = parseFloat(item.Size);
+              let finalCostPerPerson = validateCostInput(cost/size);
+              $('#groupCostPerPerson').text('Cost per person: £' + finalCostPerPerson);
+            }
+        })
+      }
+    });
+  });
+}
+
 
 
 
